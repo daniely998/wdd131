2f6cd57327bc5a8549eab3002e50959789cb0c68
daniely998 <danzone998@gmail.com>
2025-09-18 18:18:35 +0800

    place

---
+++ scripts/place.js
@@ -0,0 +1,20 @@
+const year = document.querySelector("#currentyear");
+const today = new Date();
+year.innerHTML = today.getFullYear();
+
+const lastModified = document.querySelector("#lastModified");
+lastModified.innerHTML = `Last Modified: ${new Date(document.lastModified).toLocaleString()}`;
+
+const temperature = 26;
+const conditions = "Clear";
+const windSpeed = 14;
+const windChill = calculateWindChill(temperature, windSpeed).toFixed(1);
+
+function calculateWindChill(temperature, windSpeed) {
+    return 13.12 + 0.6215 * temperature - 11.37 * windSpeed ** 0.16 + 0.3965 * temperature * windSpeed ** 0.16;
+}
+
+document.querySelector("#temperature").innerHTML = temperature;
+document.querySelector("#conditions").innerHTML = conditions;
+document.querySelector("#windspeed").innerHTML = windSpeed;
+document.querySelector("#windchill").innerHTML = windChill;
