578c1d37b62427199dccdaa416e88cae4c1b1867
daniely998 <danzone998@gmail.com>
2025-09-18 18:43:12 +0800

    wind chill update

--- scripts/place.js
+++ scripts/place.js
@@ -8,7 +8,14 @@ lastModified.innerHTML = `Last Modified: ${new Date(document.lastModified).toLoc
 const temperature = 26;
 const conditions = "Clear";
 const windSpeed = 14;
-const windChill = calculateWindChill(temperature, windSpeed).toFixed(1);
+let windChill;
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    windChill = `${calculateWindChill(temperature, windSpeed).toFixed(1)}°C`;
+}
+else {
+    windChill = "N/A";
+}
 
 function calculateWindChill(temperature, windSpeed) {
     return 13.12 + 0.6215 * temperature - 11.37 * windSpeed ** 0.16 + 0.3965 * temperature * windSpeed ** 0.16;
